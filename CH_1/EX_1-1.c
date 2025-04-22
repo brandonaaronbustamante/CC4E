a128afb1393834c7e776e32236fb2ef139d33d86
brandonaaronbustamante <me@bab.works>
2025-04-21 21:48:30 -0600

    EX1-1 complete

---
+++ CH_1/EX_1-1.c
@@ -0,0 +1,16 @@
+/**********************************************************************
+*   Author 
+*   Brandon A Bustamante
+*   License
+*   MIT (see LICENSE file)
+*   Problem
+*   K&R Exercise 1-1. Please write a program to print "Hello world"
+**********************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World\n");
+    return 0;
+}
