7467368259a1c2f49f71466361e23ad5f4745574
brandonaaronbustamante <me@bab.works>
2025-05-13 21:45:08 -0600

    Added exercise instructions for EX_2-02.c

--- CH_02/EX_2-02.c
+++ CH_02/EX_2-02.c
@@ -4,5 +4,13 @@
 *   License
 *   MIT (see LICENSE file)
 *   Problem
+*   K&R Exercise 2-2. You should write a function (no #include 
+*   statements are needed) called htoi(str) that converts hexadecimal 
+*   constant (base-16 0-9 and a-f) to an integer. There will not be a
+*    '0x' prefix (like in C) - just assume the input is a hex number. 
+*   You should not use ctype.h and your code can assume the ASCII 
+*   character set. Each time you run the program, the values you need 
+*   to convert 9e83e (base-16) to 649278 (base-10) may be different 
+*   each time you run the code.
 *   Expected Output
 **********************************************************************/
